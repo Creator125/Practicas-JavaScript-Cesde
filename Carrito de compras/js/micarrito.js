a67162e6e46205c6c71006a7e0311a9c04359bbe
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-05-09 15:15:34 -0500

    Carrito de compras/: Se modificó index.html y se agregó micarrito.js

---
+++ Carrito de compras/js/micarrito.js
@@ -0,0 +1,24 @@
+//Definir el arreglo de objeto del carrito
+let carrito = []
+
+function agregarCarrito(id, descripcion){
+    //Buscar el id en el arreglo de objeto carrito
+    let findItem = carrito.find(item => item.id == id);
+    if(findItem == undefined){
+        carrito.push({
+            id: id,
+            descripcion: descripcion,
+            cantidad: 1
+        });
+    }else{
+        //Desetructurando el objeto el findItem
+        let (id, descripcion, cantidad) = findItem;
+        cantidad++;
+        //Encontrar el indice del item arreglo
+        const index = carrito.find(item => item.id == id);
+        //Actualizar el item
+        carrito[index].cantidad = cantidad;
+    }
+      console.log(carrito);  
+
+}
