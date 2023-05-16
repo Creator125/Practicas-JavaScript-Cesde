2497a5864d52bb3337e992205c024da60feb344b
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-05-16 17:51:53 -0500

    Carrito de compras/: Desatrasado

---
+++ Carrito de compras/js/items.js
@@ -0,0 +1,93 @@
+//Refereciando los elementos
+let contitems = document.getElementById("contitems");
+contitems.textContent = `(${(0)})`;
+let carnePollo = document.getElementById("carne-pollo");
+let pollodes = document.getElementById("pollodes");      
+let carneCerdo = document.getElementById("carne-cerdo");
+let cerdodes = document.getElementById("cerdodes");      
+let carneRes = document.getElementById("carne-res");
+let resdes = document.getElementById("resdes");      
+let uvas = document.getElementById("uvas");
+let uvasdes = document.getElementById("uvasdes");      
+let manzanas = document.getElementById("manzanas");
+let repollo = document.getElementById("repollo");
+let manzanasdes = document.getElementById("manzanasdes");
+let repollodes = document.getElementById("repollodes");
+let verCarrito = document.getElementById("verCarrito");
+let mostrarCarrito = document.getElementById("mostrarCarrito");
+
+function mirarCarrito(){
+  if(carrito.length > 0){
+    // Crear una variable tipo string para almacenar todos los items del carrito de compra (table)
+    let carritoTabla = `<table class = "table table-hover">
+                            <thead>
+                              <th>Descripción</th>
+                              <th>Cantida</th>
+                              <th>Precio</th>
+                              <th>Subtotal</th>
+                            </thead>
+                            <tbody>`
+                            let total = 0; 
+                            for(item of carrito){
+                                carritoTabla += `<tr>
+                                                <td>${item.descricion}</td>
+                                                <td>${item.cantidad}</td>
+                                                <td>${item.precio}</td
+                                                <td>${item.precio * item.cantidad}</td>
+                                                <td><button type="button" class="btn btn-danger"><img src="img/icons/trash-fill.svg"></button></td>
+                                              </tr>`
+                            }                  total += item.precio * item.cantidad;
+                              carritoTabla += `</tbody>
+                                                <tfoot>
+                                              <tr>
+                                                <td colspan="4" aling="right" style="font-size:28; font-weight:bold">Total: ${total}</td>
+                                              </tr>
+                                              </tfoot>
+                                              </table>
+                                              <button type="button" class="btn btn-danger" onclik="limpiarCarrito()">Vaciar carrito`;
+    mostrarCarrito.innerHTML = carritoTabla;            
+  }else{
+    verCarrito.textContent = "El carrito está vacio"
+  }
+}
+
+//Boton de "verCarrito"
+verCarrito.addEventListener("click", mirarCarrito)
+
+//Boton de los items del mercado
+carnePollo.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("carne de pollo", pollodes.textContent, 4000);
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
+carneCerdo.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("carne de cerdo", cerdodes.textContent,5000 );
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
+carneRes.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("carne de res", resdes.textContent, 5000);
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
+uvas.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("uvas", uvasdes.textContent, 3000);
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
+manzanas.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("manzanas", manzanasdes.textContent, 4000);
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
+repollo.addEventListener('click', () => {
+    //Invocar la funcion para agreagar el intem al arreglo carrito, si existe que aumenta la cantidad
+    agregarCarrito("carne de res", repollodes.textContent, 5000);
+    //Actualizar el elemento contitems que contendrá la logitud del arreglo carrito
+    contitems.textContent = `(${(carrito.length)})`;
+});
--- Carrito de compras/js/micarrito.js
+++ Carrito de compras/js/micarrito.js
@@ -3,7 +3,7 @@ let carrito = []
 
 function agregarCarrito(id, descripcion, precio){
     //Buscar el id en el arreglo de objeto carrito
-    let findItem = carrito.find(item => item.id == id);
+    let findItem = carrito.find(item => item.id === id);
     if(findItem == undefined){
         carrito.push({
             id: id,
@@ -16,10 +16,50 @@ function agregarCarrito(id, descripcion, precio){
         let (id, descripcion, cantidad) = findItem;
         cantidad++;
         //Encontrar el indice del item arreglo
-        const index = carrito.find(item => item.id == id);
+        const index = carrito.findIndex(item => item.id === id);
         //Actualizar el item
         carrito[index].cantidad = cantidad;
     }
-      console.log(carrito);  
+      console.log(carrito);
+    
+      if(carrito.length > 0){
+        // Crear una variable tipo string para almacenar todos los items del carrito de compra (table)
+        let carritoTabla = `<table class = "table table-hover">
+                                  <thead>
+                                    <th>Descripción</th>
+                                    <th>Cantida</th>
+                                    <th>Precio</th>
+                                    <th>Subtotal</th>
+                                  </thead>
+                                  <tbody>`
+                                  let total = 0; 
+                                  for(item of carrito){
+                                      carritoTabla += `<tr>
+                                                      <td>${item.descricion}</td>
+                                                      <td>${item.cantidad}</td>
+                                                      <td>${item.precio}</td
+                                                      <td>${item.precio * item.cantidad}</td>
+                                                      <td><button type="button" class="btn btn-danger"><img src="img/icons/trash-fill.svg"></button></td>
+                                                    </tr>`
+                                  }                  total += item.precio * item.cantidad;
+                                    carritoTabla += `</tbody>
+                                                      <tfoot>
+                                                    <tr>
+                                                      <td colspan="4" aling="right" style="font-size:28; font-weight:bold">Total: ${total}</td>
+                                                    </tr>
+                                                    </tfoot>
+                                                    </table>
+                                                    <button type="button" class="btn btn-danger" onclik="limpiarCarrito()">Vaciar carrito`;
+          mostrarCarrito.innerHTML = carritoTabla;
+      }else{
+        verCarrito.textContent = "El carrito está vacio"
+      }
 
+}
+
+function limpiarCarrito(){
+    carrito = [];
+    verCarrito.innerHTML = "";
+    console.log(carrito);
+    contitems.textContent = `(${(0)})`;
 }
