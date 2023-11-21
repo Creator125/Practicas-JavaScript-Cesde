9db92b252b3acbcf6497d89e303f3058788c6b44
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-11-21 18:48:06 -0500

    Nuevas practicas inconclusa

---
+++ Semestre_3/proyect-productos/proyect-react/src/components/ShowProducts.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import axion from 'axion';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { show_alerta } from '../functions';
+
+const ShowProducts = () => {
+
+    const url = "https://api-proucts.run"
+    const [products, setProducts] = useState([]);
+    const [id, setId] = useState('');
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ShowProducts
